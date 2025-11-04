744fc90de095dfd0f359958e207e39ec3b84c876
Jackie264 <125248570+Jackie264@users.noreply.github.com>
2025-11-05 07:00:55 +0800

    Update base.lua

--- luasrc/model/cbi/AdGuardHome/base.lua
+++ luasrc/model/cbi/AdGuardHome/base.lua
@@ -6,7 +6,7 @@ local fs = require"nixio.fs"
 local uci = require"luci.model.uci".cursor()
 local configpath = uci:get("AdGuardHome","AdGuardHome","configpath") or "/etc/AdGuardHome.yaml"
 local binpath = uci:get("AdGuardHome", "AdGuardHome", "binpath") or "/usr/bin/AdGuardHome"
-httpport = uci:get("AdGuardHome","AdGuardHome","httpport") or "3000"
+httpport = uci:get("AdGuardHome","AdGuardHome","httpport") or "3008"
 m = Map("AdGuardHome", "AdGuard Home")
 m.description = translate("Free and open source, powerful network-wide ads & trackers blocking DNS server.")
     .. [[<style>
@@ -29,8 +29,8 @@ o = s:option(Flag, "enabled", translate("Enable"))
 o.default = 0
 o.optional = false
 o = s:option(Value,"httpport",translate("Browser management port"))
-o.placeholder = 3000
-o.default = 3000
+o.placeholder = 3008
+o.default = 3008
 o.datatype = "port"
 o.optional = false
 o.description = translate(
@@ -39,11 +39,11 @@ o.description = translate(
   "value='AdGuardHome Web:" .. httpport .. "' " ..
   "onclick=\"window.open('http://' + window.location.hostname + ':" .. httpport .. "')\"/>"
 )
-local binmtime=uci:get("AdGuardHome","AdGuardHome","binmtime") or "0"
+local binmtime = uci:get("AdGuardHome","AdGuardHome","binmtime") or "0"
 local e = ""
 if not fs.access(configpath) then e = e .. " " .. translate("no config") end
 if not fs.access(binpath) then
-	e = e.." "..translate("no core")
+	e = e .. " " .. translate("no core")
 else
 	local version = uci:get("AdGuardHome","AdGuardHome","version")
 	local testtime = fs.stat(binpath,"mtime")
@@ -54,7 +54,7 @@ else
         uci:set("AdGuardHome", "AdGuardHome", "binmtime", testtime)
         uci:commit("AdGuardHome")
 	end
-	e = version..e
+	e = version .. e
 end
 
 o = s:option(ListValue, "core_version", translate("Core Version"))
@@ -68,7 +68,7 @@ o.showfastconfig = (not fs.access(configpath))
 o.description = string.format(translate("Current core version:") .. "<strong><font id='updateversion' style=\'color:green\'>%s </font></strong>", e)
 local portcommand = "awk '/port:/ && ++count == 2 {sub(/[^0-9]+/, \"\", $2); printf(\"%s\\n\", $2); exit}' " .. configpath .. " 2>nul"
 local port = luci.util.exec(portcommand)
-if (port=="") then port="?" end
+if (port == "") then port = "?" end
 o = s:option(ListValue, "redirect", port..translate("Redirect"), translate("AdGuardHome redirect mode"))
 o.placeholder = "none"
 o:value("none", translate("none"))
@@ -123,7 +123,7 @@ o.validate = function(self, value)
 if value == "" then return nil end
 if fs.stat(value,"type") == "reg" then
 	if m.message then
-	m.message = m.message.."\nerror!work dir is a file"
+	m.message = m.message .. "\nerror!work dir is a file"
 	else
 	m.message = "error!work dir is a file"
 	end
@@ -144,7 +144,7 @@ if fs.stat(value,"type") == "dir" then
 end
 if fs.stat(value,"type") == "dir" then
 	if m.message then
-	m.message = m.message.."\nerror!log file is a dir"
+	m.message = m.message .. "\nerror!log file is a dir"
 	else
 	m.message = "error!log file is a dir"
 	end
@@ -155,8 +155,8 @@ end
 o = s:option(Flag, "verbose", translate("Verbose log"))
 o.default = 0
 o.optional = true
-local a = luci.sys.call("grep -m 1 -q programadd "..configpath)
-if (a==0) then
+local a = luci.sys.call("grep -m 1 -q programadd " .. configpath)
+if (a == 0) then
 a = "Added"
 else
 a = "Not added"
@@ -175,7 +175,7 @@ o.write = function()
 	luci.sys.exec("sh /usr/share/AdGuardHome/gfw2adg.sh 2>&1")
 	luci.http.redirect(luci.dispatcher.build_url("admin","services","AdGuardHome"))
 end
-o = s:option(Value, "gfwupstream", translate("Gfwlist upstream dns server"), translate("Gfwlist domain upstream dns service")..translate(a))
+o = s:option(Value, "gfwupstream", translate("Gfwlist upstream dns server"), translate("Gfwlist domain upstream dns service") .. translate(a))
 o.default = "tcp://208.67.220.220:5353"
 o.datatype = "string"
 o.optional = true
@@ -226,7 +226,7 @@ o1.optional = false
 o1.validate = function(self, value)
 if fs.stat(value,"type") == "reg" then
 	if m.message then
-	m.message = m.message.."\nerror!backup dir is a file"
+	m.message = m.message .. "\nerror!backup dir is a file"
 	else
 	m.message = "error!backup dir is a file"
 	end
