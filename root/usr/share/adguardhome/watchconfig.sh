c867453dc5772a344e75ba86d8b09a00af9b9523
Jackie264 <125248570+Jackie264@users.noreply.github.com>
2025-12-17 11:41:17 +0800

    Update and rename watchconfig.sh to watchconfig.sh

--- root/usr/share/adguardhome/watchconfig.sh
+++ root/usr/share/adguardhome/watchconfig.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 PATH="/usr/sbin:/usr/bin:/sbin:/bin"
-configpath=$(uci get AdGuardHome.AdGuardHome.configpath)
+configpath=$(uci get adguardhome.adguardhome.configpath)
 while :
 do
 	sleep 10
 	if [ -f "$configpath" ]; then
-		/etc/init.d/AdGuardHome do_redirect 1
+		/etc/init.d/adguardhome do_redirect 1
 		break
 	fi
 done
