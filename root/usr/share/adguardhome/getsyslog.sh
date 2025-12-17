ac74df7a7dae390685052c396fd9b34600ec54e0
Jackie264 <125248570+Jackie264@users.noreply.github.com>
2025-12-17 11:47:38 +0800

    Update getsyslog.sh

--- root/usr/share/adguardhome/getsyslog.sh
+++ root/usr/share/adguardhome/getsyslog.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 PATH="/usr/sbin:/usr/bin:/sbin:/bin"
-logread -e AdGuardHome > /tmp/AdGuardHometmp.log
-logread -e AdGuardHome -f >> /tmp/AdGuardHometmp.log &
+logread -e AdGuardHome > /tmp/adguardhometmp.log
+logread -e AdGuardHome -f >> /tmp/adguardhometmp.log &
 pid=$!
-echo "1">/var/run/AdGuardHomesyslog
+echo "1">/var/run/adguardhomesyslog
 while true
 do
 	sleep 12
-	watchdog=$(cat /var/run/AdGuardHomesyslog)
+	watchdog=$(cat /var/run/adguardhomesyslog)
 	if [ "$watchdog"x == "0"x ]; then
 		kill $pid
-		rm /tmp/AdGuardHometmp.log
-		rm /var/run/AdGuardHomesyslog
+		rm /tmp/adguardhometmp.log
+		rm /var/run/adguardhomesyslog
 		exit 0
 	else
-		echo "0">/var/run/AdGuardHomesyslog
+		echo "0">/var/run/adguardhomesyslog
 	fi
 done
